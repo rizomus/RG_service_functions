b55df14b1add373ac9600c3b11cfdcc291d91ce9
rizomus <104506812+rizomus@users.noreply.github.com>
2022-08-31 16:33:24 +0300

    import RandomForestRegressor

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.losses import MAE, MSE
 from tensorflow.keras.losses import mean_absolute_percentage_error as MAPE
 import tensorflow.keras.backend as K
+from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import numpy as np
 
